cc4daaaf49c70de4b21fda65b330da40cefbbb0e
Maurício Henrique <mauricio.henrique@4all.com>
2019-12-11 13:33:54 -0300

    menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,10 +4,12 @@ import "./styles/Menu.css";
 
 const Menu = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light menu">
+    <nav className="navbar navbar-expand-lg navbar-light menu">
       <div className="container">
         <Link to="/" className="navbar-brand" href="#">
-          BRAND
+          <h4>
+            OB<span>J</span>
+          </h4>
         </Link>
         <button
           className="navbar-toggler"
@@ -18,50 +20,55 @@ const Menu = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <i class="material-icons">menu</i>
         </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav float-right">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/home">
-                Home <span className="sr-only">(current)</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/home">
-                Wish
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">
-                  Action
+        <div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav float-right">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/home">
+                  Home <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/home">
+                  Wishes
+                </Link>
+              </li>
+              {/* <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown
                 </a>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </div>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li>Login</li>
-          </ul>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <div className="dropdown-divider"></div>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </li> */}
+            </ul>
+            <ul className="navbar-nav">
+              <li>
+                <Link className="nav-link" to="/loign">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
