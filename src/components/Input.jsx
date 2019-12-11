7e55e2d31bab46f2c7bd0fc55dc9b76b5302c5e6
Maurício Henrique <mauricio.henrique@4all.com>
2019-12-11 17:59:51 -0300

    input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,23 +1,54 @@
 import React from "react";
 import "./styles/Input.css";
 
-const Input = props => {
-  return (
-    <div className="input-container">      
-      <form>
+class Input extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      required: props.required,
+      label: props.label,
+      type: props.type,
+      placeholder: props.placeholder,
+      value: "",
+      field: props.field,
+      updateValue: props.updateValue,
+      error: false,
+    };
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+    this.state.updateValue(this.state.value, this.state.field);
+  }
+
+  handleValidate(event) {
+    const value = event.target.value;
+    console.log(value);
+    if (!value) {
+      this.setState({error: true})
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="input-container">
         <div className="form-group">
           <label>
-            {props.label} {props.required && <span>*</span>}
+            {this.state.label} {this.state.required && <span>*</span>}
           </label>
           <input
-            type={props.type}
-            className="form-control"
-            placeholder={props.placeholder}
+            type={this.state.type}
+            className={`form-control ${this.state.error ? 'input-error' : 'remove-border'}`}
+            placeholder={this.state.placeholder}
+            value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            onBlur={this.handleValidate.bind(this)}
           />
         </div>
-      </form>
-    </div>
-  );
-};
+      </div>
+    );
+  }
+}
 
 export default Input;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,11 +2,32 @@ import React from "react";
 import Input from "../components/Input";
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      form: {
+        wish: ""
+      }
+    };
+  }
+
+  handleUpdate(value, field) {
+    this.setState({ form: { [field]: value } });
+  }
+
   render() {
     return (
       <div className="mt-5">
         <h2>Whishes</h2>
-        <Input placeholder="Inform yout wish" label="Wish" required="true"/>
+        <form>
+          <Input
+            placeholder="Inform yout wish"
+            label="Wish"
+            required="true"
+            field="wish"
+            updateValue={this.handleUpdate.bind(this)}
+          />
+        </form>
       </div>
     );
   }
